de7fc0425637792e7e83b77e90cb971af105889e
Anson Yu Wang <opensource@undozen.com>
2015-03-26 09:51:38 +0800

    start to work

---
+++ app.js
@@ -0,0 +1,73 @@
+'use strict';
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var co = require('co');
+var browserify = require('browserify');
+var UglifyJs = require('uglify-js');
+global.Promise = require('bluebird');
+var env = process.env.NODE_ENV || 'development';
+var ON_PRODUCTION = env === 'production';
+
+var server = http.createServer();
+
+var getClientSource = cachePromise(function (resolve, reject) {
+    fs.readFile(path.join(__dirname, 'client.html'), 'utf-8',
+        function (err, content) {
+            if (err) return reject(err);
+            resolve(content);
+        });
+});
+
+var getClientScript = cachePromise(function (resolve, reject) {
+    var b = browserify({
+        basedir: __dirname
+    });
+    b.add('./browser.js');
+    b.bundle(function (err, source) {
+        if (err) return reject(err);
+        source = source.toString('utf-8');
+        if (ON_PRODUCTION) {
+            try {
+                source = UglifyJs.minify(source, {
+                    fromString: true
+                }).code;
+            } catch (e) {
+                return reject(e);
+            }
+            resolve(source);
+        } else {
+            resolve(source);
+        }
+    });
+});
+
+function cachePromise(fn) {
+    var cache;
+
+    function np() {
+        return new Promise(fn);
+    }
+    if (ON_PRODUCTION) {
+        return function () {
+            return cache ? cache : (cache = np());
+        }
+    } else {
+        return function () {
+            return np();
+        }
+    }
+};
+
+server.on('request', co.wrap(function * (req, res) {
+    req.url = req.url.replace(/\/+/g, '/');
+    if (req.url.indexOf('/browser.js') === 0) {
+        res.setHeader('Content-Type',
+            'application/javascript; charset=utf-8');
+        return res.end(yield getClientScript());
+    }
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(yield getClientSource());
+}));
+
+server.listen(28693);
